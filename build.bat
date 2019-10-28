97f8d9d4716bff892fa5c5ba8575a73016aec4a7
ISSOtm <eldredhabert0@gmail.com>
2019-10-28 15:46:50 +0100

    Fix Windows build script

--- build.bat
+++ build.bat
@@ -3,14 +3,14 @@
 mkdir obj
 mkdir bin
 
-rgbasm -p 0xFF -h -i src/ -i src/include/ -i src/hUGE/ -o obj\music.o      src\music.asm
+rgbasm -p 0xFF -h -i src/ -i src/include/ -i src/fortISSimO/ -o obj\music.o      src\music.asm
 if %errorlevel% neq 0 call :exit 1
-rgbasm -p 0xFF -h -i src/ -i src/include/ -i src/hUGE/ -o obj\main.o       src\main.asm
+rgbasm -p 0xFF -h -i src/ -i src/include/ -i src/fortISSimO/ -o obj\main.o       src\main.asm
 if %errorlevel% neq 0 call :exit 1
 
-rgblink -p 0xFF-d  -m bin\hUGEDriver.map -n bin\hUGEDriver.sym -o bin\hUGEDriver.gb obj\music.o obj\main.o
+rgblink -p 0xFF-d  -m bin\example.map -n bin\example.sym -o bin\example.gb obj\music.o obj\main.o
 if %errorlevel% neq 0 call :exit 1
-rgbfix -p 0xFF -v -i HUGE -k HB -l 0x33 -m 0 -n 0 -r 0 -t hUGEDriver bin\hUGEDriver.gb
+rgbfix -p 0xFF -v -i HUGE -k HB -l 0x33 -m 0 -n 0 -r 0 -t hUGEDriver bin\example.gb
 if %errorlevel% neq 0 call :exit 1
 call :exit 0
 
