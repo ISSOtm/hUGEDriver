64e100aa3a0e26bf104b6a58c6d6852f97a27009
ISSOtm <eldredhabert0@gmail.com>
2023-02-21 00:48:39 +0100

    Fix `build.bat`

--- build.bat
+++ build.bat
@@ -3,16 +3,17 @@
 mkdir obj
 mkdir bin
 
-rgbasm -p 0xFF -h -i src/ -i src/include/ -i src/fortISSimO/ -o obj\music.o      src\music.asm
+rgbgfx -o obj/chicago8x8.2bpp @src/chicago8x8.flags src/chicago8x8.png
 if %errorlevel% neq 0 call :exit 1
-rgbasm -p 0xFF -h -i src/ -i src/include/ -i src/fortISSimO/ -o obj\main.o       src\main.asm
+rgbasm -h -p 0xFF -i src/include/ -i src/fortISSimO/ -o obj\music.o      src\music.asm
+if %errorlevel% neq 0 call :exit 1
+rgbasm -h -p 0xFF -i src/include/ -i src/fortISSimO/ -o obj\main.o       src\main.asm
 if %errorlevel% neq 0 call :exit 1
 
-rgblink -p 0xFF-d  -m bin\example.map -n bin\example.sym -o bin\example.gb obj\music.o obj\main.o
+rgblink -p 0xFF -d -m bin\example.map -n bin\example.sym -o bin\example.gb obj\music.o obj\main.o
 if %errorlevel% neq 0 call :exit 1
-rgbfix -p 0xFF -v -i HUGE -k HB -l 0x33 -m 0 -n 0 -r 0 -t hUGEDriver bin\example.gb
+rgbfix -p 0xFF -v bin\example.gb
 if %errorlevel% neq 0 call :exit 1
 call :exit 0
 
 :exit
-exit
